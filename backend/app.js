755e3cf43792269fbf680a8badda079fc3ad19b9
Soufflet Elodie <soufflet.elodi@gmail.com>
2024-06-27 14:50:52 +0200

    main / suppression d'un fichier. Création d'un fichier .env et téléchargement du package dotenv

--- backend/app.js
+++ backend/app.js
@@ -1,8 +1,7 @@
+// importe les modules nécessaires
 const express = require('express');
 
 const mongoose = require('mongoose');
-
-const bodyParser = require('body-parser');
 
 const path = require('path');
 
@@ -10,16 +9,22 @@ const bookRoutes = require('./routes/book');
 
 const userRoutes = require('./routes/user');
 
-mongoose.connect('mongodb+srv://test:TDplMZ3ZxOVUeyPs@cluster0.hpbqxtx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
+require('dotenv').config();
+
+// connexion à mongoose la base de données
+mongoose.connect(`mongodb+srv://${process.env.MONGODB_USERNAME}:${process.env.MONGODB_PASSWORD}@cluster0.hpbqxtx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`)
   .then(() => console.log('Connexion à MongoDB réussie !'))
   .catch((error) => {
     console.log('Connexion à MongoDB échouée %o !', error);
   });
 
+// création d'une application express
 const app = express();
 
+// middleware pour analyser les corps des requêtes entrante en JSON
 app.use(express.json());
 
+// middleware CORS. on définit les en-têtes de réponse HTTP pour gérer les CORS
 app.use((req, res, next) => {
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
@@ -27,10 +32,10 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(bodyParser.json());
-
+// on associe les routes aux bonnes URI
 app.use('/api/books', bookRoutes);
 app.use('/api/auth', userRoutes);
+// permet d'aller récupérer les images contenue dans le dossier images
 app.use('/images', express.static(path.join(__dirname, 'images')));
 
 module.exports = app;
